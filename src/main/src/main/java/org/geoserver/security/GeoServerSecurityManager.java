e696f509b6b8a3c0a76e59efaecc7cb8d794a02a
Gabriel Roldan <gabriel.roldan@gmail.com>
2021-09-21 00:44:26 -0300

    GeoServerSecurityManager.reload() does not clear caches
    GeoServerSecurityManager holds internal caches that are not cleared when
    the configuration is reloaded:
    * userGroupServices
    * roleServices
    * passwordValidators

--- src/main/src/main/java/org/geoserver/security/GeoServerSecurityManager.java
+++ src/main/src/main/java/org/geoserver/security/GeoServerSecurityManager.java
@@ -574,8 +574,7 @@ public class GeoServerSecurityManager implements ApplicationContextAware, Applic
                 GeoServerExtensions.extensions(GeoServerSecurityProvider.class)) {
             securityProvider.destroy(this);
         }
-        userGroupServices.clear();
-        roleServices.clear();
+        clearCaches();
 
         userGroupServiceHelper.destroy();
         roleServiceHelper.destroy();
@@ -607,9 +606,16 @@ public class GeoServerSecurityManager implements ApplicationContextAware, Applic
      * loads configuration and initializes the security subsystem.
      */
     void init() throws Exception {
+        clearCaches();
         init(loadMasterPasswordConfig());
         init(loadSecurityConfig());
         fireChanged();
+    }
+
+    private void clearCaches() {
+        userGroupServices.clear();
+        roleServices.clear();
+        passwordValidators.clear();
     }
 
     synchronized void init(SecurityManagerConfig config) throws Exception {
